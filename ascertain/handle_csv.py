27781973079a004340ead9109f64433267713aec
Aleksei Khatkevich <alekseikhatkevich@gmail.com>
2020-11-06 16:09:54 +0300

    скрипт для закачки csv -1

--- ascertain/handle_csv.py
+++ ascertain/handle_csv.py
@@ -1,41 +1,79 @@
-from telephone_numbers import constants
 import asyncio
+from pathlib import Path
+
+import aiofiles
 import aiohttp
+from rest_framework import status
+import datetime
+from telephone_numbers import constants
+
+
+#RuntimeError: Event loop is closed
+# аннотация
+# описание методов
 
 
 class Download_CSV:
     """
     Скачивает CSV файлы с данными телефонных номеров.
     """
-    urls = constants.CSV_URLS
 
-    @staticmethod
-    async def download_one_csv(
-            session: aiohttp.ClientSession,
-            semaphore: asyncio.Semaphore
-    ) -> tuple:
+    def __init__(self,
+                 urls=constants.CSV_URLS,
+                 path=Path('ascertain/csv_files'),
+                 ) -> None:
+        self.urls = urls
+        self.path = path
+
+    def get_path(self, url):
         """
-        Checks response status on HEAD request being sent to url address.
         """
-        async with semaphore, session.get(series.imdb_url) as response:
+        return self.path / url.split('/')[-1]
+
+    async def download_one_csv(self, session: aiohttp.ClientSession, url: str):
+        """
+        Скачивает один CSV файл.
+        """
+        async with session.get(url) as response:
+            await self.write_one_file(response, url)
+
             return response
+            # if response.status == status.HTTP_200_OK:
+            #     async with aiofiles.open(self.get_path(url), mode='wb') as file:
+            #         while True:
+            #             chunk = await response.content.read(1000)
+            #             if not chunk:
+            #                 break
+            #             await file.write(chunk)
 
-    async def get_status(
-            self,
-            max_workers: int = 20,
-            response_timeout: int = 7,
-    ) -> Generator[series_instance, None, None]:
+    async def write_one_file(self, response, url):
         """
-        Runs HEAD requests in event loop asynchronously, then constricts generator with
-        series, status pairs if pair is not an exception.
         """
-        semaphore = asyncio.Semaphore(max_workers)
+        if response.status == status.HTTP_200_OK:
+            async with aiofiles.open(self.get_path(url), mode='wb') as file:
+                while True:
+                    chunk = await response.content.read(1000)
+                    if not chunk:
+                        break
+                    await file.write(chunk)
 
+    async def download_all_csv(self):
+        """
+        """
         async with aiohttp.ClientSession() as session:
-            series_to_statuses = await asyncio.gather(
-                *(asyncio.wait_for(self.head_status(session, series, semaphore), response_timeout)
-                  for series in await self.get_queryset()
-                  ), return_exceptions=True,
+            mutual_content = await asyncio.gather(
+                *[self.download_one_csv(session, url)for url in self.urls],
+                return_exceptions=True,
             )
 
-        return (pair for pair in series_to_statuses if not isinstance(pair, Exception))
+        return mutual_content
+
+    def __call__(self, *args, **kwargs):
+        """
+
+        """
+        a =datetime.datetime.now()
+        mutual_content = asyncio.run(self.download_all_csv())
+        delta = datetime.datetime.now() -a
+        print(delta)
+        return mutual_content
