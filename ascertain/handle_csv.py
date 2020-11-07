538247b951b7f395464202b75d2f88750484a023
Aleksei Khatkevich <alekseikhatkevich@gmail.com>
2020-11-07 09:27:46 +0300

    скрипт для закачки csv - зредварительно закончен

--- ascertain/handle_csv.py
+++ ascertain/handle_csv.py
@@ -1,6 +1,6 @@
 import asyncio
 from pathlib import Path
-from typing import Iterable, Union, Container
+from typing import Container, Iterable, Optional, Union
 
 import aiofiles
 import aiohttp
@@ -10,22 +10,20 @@ from rest_framework import status
 from telephone_numbers import constants
 
 
-#'Last-Modified': 'Fri, 06 Nov 2020 00:00:02 GMT', 'Etag': '"5fa49202-bded3"'  JsonField?
-#RuntimeError: Event loop is closed
-# аннотация
-# описание методов
-# валидация
-
 class Download_CSV:
     """
     Скачивает CSV файлы с данными телефонных номеров.
+    rossvyaz.gov.ru обновляет данные файлы каждый день в 12 ночи, изменяя 'etag', 'last-modified'
+    (проверял лично) и пр. Поэтому при загрузке CSV в базу по ночам раз в сутки клиентское кеширование
+     не имеет смысла. Все равно 'etag' каждый раз новый и мы реально не знаем обновился ли файл и 'etag'
+     или просто 'etag'.
     """
 
     def __init__(self,
                  urls: Iterable = constants.CSV_URLS,
                  path: Union[Path, str] = Path('ascertain/csv_files'),
                  chunk_size: int = 100000,
-                 response_timeout: Union[float, int] = 60,
+                 response_timeout: Optional[Union[float, int]] = 60,
                  ) -> None:
         self.urls = urls
         self.path = Path(path)
@@ -34,7 +32,7 @@ class Download_CSV:
 
     def get_path(self, url: str) -> Path:
         """
-        Создает путь к записываемому csv файлу путем конкатенации path и последней части урла.
+        Создает путь к записываемому csv файлу путем конкатенации 'path' и последней части 'url''.
         'ascertain/csv_files' + 'ABC-3xx.csv' = 'ascertain/csv_files/ABC-3xx.csv'
         """
         return self.path / url.split('/')[-1]
@@ -45,9 +43,12 @@ class Download_CSV:
                                ) -> ClientResponse:
         """
         Получает содержимое CSV файла с сервера и записывает его в файл.
+        В случае любого статуса за исключением 200 рейзит HttpProcessingError.
         """
         async with session.get(url) as response:
-            await self.write_one_file(response, url)
+
+            if response.status == status.HTTP_200_OK:
+                await asyncio.create_task(self.write_one_file(response, url))
 
             return response
 
@@ -55,21 +56,21 @@ class Download_CSV:
         """
         Записывает полезную нагрузку респонса в файл потоково кусками по 'chunk_size'.
         """
-        if response.status == status.HTTP_200_OK:
-            async with aiofiles.open(self.get_path(url), mode='wb') as file:
-                while True:
-                    chunk = await response.content.read(self.chunk_size)
-                    if not chunk:
-                        break
-                    else:
-                        await file.write(chunk)
+        async with aiofiles.open(self.get_path(url), mode='wb') as file:
+
+            while True:
+                chunk = await response.content.read(self.chunk_size)
+                if not chunk:
+                    break
+                else:
+                    await file.write(chunk)
 
     async def download_all_csv(self) -> Container:
         """
         Запускает загрузку и сохранение на диск всех файлов.
         """
         async with aiohttp.ClientSession() as session:
-            mutual_content = await asyncio.gather(
+            mutual_response = await asyncio.gather(
                 *[
                     asyncio.wait_for(
                         self.download_one_csv(session, url), self.response_timeout
@@ -78,15 +79,16 @@ class Download_CSV:
                 return_exceptions=True,
             )
 
-        return mutual_content
+        return mutual_response
 
-    def __call__(self, *args, **kwargs) -> Container:
+    def __call__(self, *args, **kwargs) -> tuple:
         """
         Запускает весь процесс.
-        1 -
-        2 -
-        3 - 
+        1 - Ассинхронно загружаем csv файлы.
+        2 - Ассинхронно записываем их на диск.
         """
-        mutual_content = asyncio.run(self.download_all_csv())
+        mutual_response = asyncio.run(self.download_all_csv())
 
-        return mutual_content
+        return mutual_response
+
+
